23f5fd440ea0a3f7dda969df4670fd51baa8ff89
Yusuf Muradov <surgeon33NN@yandex.ru>
2025-04-06 18:59:23 +0300

    try another way of response status

--- src/main/java/ru/yandex/practicum/filmorate/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.exception;
 
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,14 +12,16 @@ import ru.yandex.practicum.filmorate.model.ErrorResponse;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(NotFoundException.class)
-    public ErrorResponse handleNotFoundException(final NotFoundException e) {
-        return new ErrorResponse(NotFoundException.class.getSimpleName(), e.getMessage());
-    }
-
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({ValidationException.class, MethodArgumentNotValidException.class})
     public ErrorResponse handleValidationException(final Exception e) {
         return new ErrorResponse(ValidationException.class.getSimpleName(), e.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ErrorResponse handleNotFoundException(final NotFoundException e) {
+        return new ErrorResponse(NotFoundException.class.getSimpleName(), e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
