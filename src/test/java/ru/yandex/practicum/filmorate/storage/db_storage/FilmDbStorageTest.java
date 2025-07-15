ffcf3b22e2d3543cbd8ccd8db0cc01d641d0484f
Rifnur Vakhitov <rifnur69v@yandex.ru>
2025-07-15 18:30:54 +0300

    Добавил тесты CheckStyle

--- src/test/java/ru/yandex/practicum/filmorate/storage/db_storage/FilmDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/db_storage/FilmDbStorageTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -16,7 +15,6 @@ import ru.yandex.practicum.filmorate.model.entity.Film;
 import ru.yandex.practicum.filmorate.model.entity.MpaRating;
 
 import java.time.LocalDate;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
